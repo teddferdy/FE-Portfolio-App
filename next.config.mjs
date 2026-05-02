c9c3c2bd7f92b30c81153d8a999987095c04e00d
Teddy-FN <teddy.amrullah@ist.id>
2026-05-02 16:58:42 +0700

    Delete Error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
-import nextI18NextConfig from "./next-i18next.config.js";
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["drive.google.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+    ],
   },
-  i18n: nextI18NextConfig.i18n,
 };
 
 export default nextConfig;
